cd78fc220f434ddfd769bf5b2f95ba743186b366
PsvitaNeedsCat <53197859+PsvitaNeedsCat@users.noreply.github.com>
2020-04-24 12:13:21 +1200

    Player Scripts
    Scripts added for:
    - Player
    - Player Controller
    - PlayerInput
    - Grid
    - Tile
    
    Player script still has some blanks due to no event handler.
    Still need a solution for destroying chunks nicer. Currently they do not remove themselves from list.

--- Assets/Scripts/Level/Tile.cs
+++ Assets/Scripts/Level/Tile.cs
@@ -6,28 +6,38 @@ public class Tile : MonoBehaviour
 {
     // Public variables
 
-
     // Private variables
-    [SerializeField] private eType m_type;
     [SerializeField] private GlobalTileSettings m_globalSettings;
+    private eType m_type;
 
-    public void TryRaiseChunk()
+    // Returns null if chunk failed to raise
+    public Chunk TryRaiseChunk()
     {
+        Chunk newChunk = null;
+
         // Cannot raise a type of none
-        if (m_type == eType.none) { return; }
+        if (m_type == eType.none) { return newChunk; }
 
         // Cannot raise an occupied tile
-        if (IsOccupied()) { return; }
+        if (IsOccupied()) { return newChunk; }
 
         // Spawn new chunk and raise it
         GameObject chunkPrefab = m_globalSettings.m_chunkPrefabs[(int)m_type];
-        Chunk newChunk = Instantiate(chunkPrefab, transform.position, transform.rotation, null).GetComponent<Chunk>();
+        newChunk = Instantiate(chunkPrefab, transform.position, transform.rotation, null).GetComponent<Chunk>();
         newChunk.RaiseChunk();
+
+        return newChunk;
     }
 
     // Checks if there is a chunk currently above thise tile
     public bool IsOccupied()
     {
         return Physics.Raycast(transform.position, Vector3.up, m_globalSettings.m_tileSize, m_globalSettings.m_raycastMask);
+    }
+
+    // Returns the tile's type
+    public eType GetTileType()
+    {
+        return m_type;
     }
 }
---
+++ Assets/Scripts/Settings/GlobalPlayerSettings.cs
@@ -0,0 +1,34 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+[CreateAssetMenu(fileName = "NewGlobalPlayerSettings", menuName = "Settings/GlobalPlayerSettings")]
+public class GlobalPlayerSettings : ScriptableObject
+{
+    [Header("Cooldowns")]
+    [Tooltip("How long before the player can punch again")]
+    public float m_punchCooldown = 1.0f;
+    [Tooltip("How long before the player can attempt to raise a chunk again")]
+    public float m_raiseCooldown = 1.0f;
+
+    [Header("Physics")]
+    [Tooltip("How much gravity is acting on the player")]
+    public float m_gravity = -300.0f;
+    [Tooltip("Amount of drag on player's X and Y")]
+    public float m_drag = 2.0f;
+    [Tooltip("How much force is applied to the player when moving")]
+    public float m_moveForce = 10.0f;
+
+    [Header("Tile Targeter")]
+    [Tooltip("Maximum range between tile targeter position and closest tile")]
+    public float m_maxTileRange = 5.0f;
+    [Tooltip("Minimum range between query position and tile")]
+    public float m_minTileRange = 7.0f;
+
+    [Header("Hurtbox")]
+    [Tooltip("How much horiztonally to move the hurt box by when spawning")]
+    public float m_hurtboxMoveBy = 5.0f;
+
+    [Tooltip("Maximum number of chunks allowed at once")]
+    public int m_maxChunks = 3;
+}
