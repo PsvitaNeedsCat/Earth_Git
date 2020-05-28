6b92fd5754f7213a1f04285c947d4fafdce87f27
Sebbyling <43594362+sebbyling@users.noreply.github.com>
2020-05-28 13:37:54 +1200

    Fire Scene Updates

---
+++ Assets/Materials/Shaders/CameraFade_SCR.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CameraFade_SCR : MonoBehaviour
+{
+    private Material mat;
+    private float alpha = 1.0f;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        mat = GetComponent<Material>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
