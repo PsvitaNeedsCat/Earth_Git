0fe34b1ac94f95bca3f5d2baa179b306dd2df9ae
Tim Sinclair <timbbby@gmail.com>
2020-06-10 15:12:14 +1200

    Small feedback changes

--- Assets/Scripts/Bosses/Centipede/CentipedeTailFirer.cs
+++ Assets/Scripts/Bosses/Centipede/CentipedeTailFirer.cs
@@ -1,15 +1,19 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using DG.Tweening;
 
 public class CentipedeTailFirer : MonoBehaviour
 {
+    public GameObject m_tailObject;
     public GameObject m_projectilePrefab;
     public float m_spawnDistance;
     public float m_spawnHeight;
 
     public void FireAll(bool _tailDamaged)
     {
+        m_tailObject.transform.DOPunchScale(Vector3.one * 1.5f, 0.1f);
+
         Fire(transform.localRotation * Vector3.right, _tailDamaged);
         Fire(transform.localRotation * Vector3.left, _tailDamaged);
         Fire(transform.localRotation * Vector3.forward, _tailDamaged);
