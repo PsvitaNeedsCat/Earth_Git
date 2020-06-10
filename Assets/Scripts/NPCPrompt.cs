1773b11daef4a61c044ee0455489295a9d09ecbb
Tim Sinclair <timbbby@gmail.com>
2020-06-10 23:15:30 +1200

    Added NPC Button Prompt

---
+++ Assets/Scripts/NPCPrompt.cs
@@ -0,0 +1,31 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+using DG.Tweening;
+
+public class NPCPrompt : MonoBehaviour
+{
+    public GameObject m_buttonPrompt;
+
+    private void Awake()
+    {
+        m_buttonPrompt.SetActive(false);
+    }
+
+    private void OnTriggerEnter(Collider other)
+    {
+        if (other.GetComponent<Player>())
+        {
+            m_buttonPrompt.SetActive(true);
+        }
+    }
+
+    private void OnTriggerExit(Collider other)
+    {
+        if (other.GetComponent<Player>())
+        {
+            m_buttonPrompt.SetActive(false);
+        }
+    }
+}
