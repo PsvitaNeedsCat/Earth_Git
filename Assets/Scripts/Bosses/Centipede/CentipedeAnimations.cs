cebe41a5144fc4183fef56a1c37f4e6e8013d823
Tim <timbbby@gmail.com>
2020-07-09 10:40:42 +1200

    Fixed centipede bugs

--- Assets/Scripts/Bosses/Centipede/CentipedeAnimations.cs
+++ Assets/Scripts/Bosses/Centipede/CentipedeAnimations.cs
@@ -7,18 +7,23 @@ public class CentipedeAnimations : MonoBehaviour
     public List<Animator> m_segmentAnimators;
     public AnimationCurve m_speedCurve;
 
-    private void Awake()
+    private void OnEnable()
     {
+        float startTime = 0.0f;
+
         for (int i = 0; i < m_segmentAnimators.Count; i++)
         {
-            if (i % 2 == 1)
-            {
-                m_segmentAnimators[i].Play("Walk", 0, 0.25f);
-            }
-            else
-            {
-                m_segmentAnimators[i].Play("Walk", 0, 0.0f);
-            }
+            m_segmentAnimators[i].Play("Walk", 0, startTime);
+            startTime = (startTime + 0.25f) % 1.0f;
+
+            //if (i % 2 == 1)
+            //{
+            //    m_segmentAnimators[i].Play("Walk", 0, 0.75f);
+            //}
+            //else
+            //{
+            //    m_segmentAnimators[i].Play("Walk", 0, 0.0f);
+            //}
         }
     }
 
