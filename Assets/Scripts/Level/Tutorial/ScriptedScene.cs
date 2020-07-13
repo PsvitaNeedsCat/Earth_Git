593f660d96346ee11dcb0c86f8e5586cda26883a
Callum <53197859+PsvitaNeedsCat@users.noreply.github.com>
2020-07-14 10:54:49 +1200

    Scripted Scene
    - Added script that allows for dialogue and animations to be selected
    - Script needs to be added to so that it plays them sequencially

---
+++ Assets/Scripts/Level/Tutorial/ScriptedScene.cs
@@ -0,0 +1,39 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEditor;
+
+[System.Serializable]
+public class ScriptedScene : MonoBehaviour
+{
+    public GenericEvent[] m_script;
+
+    private void Update()
+    {
+        // Play them in order
+    }
+}
+
+[System.Serializable]
+public class GenericEvent
+{
+    public enum Type
+    {
+        none,
+        dialogue,
+        animation
+    }
+
+    public Type m_type = Type.none;
+}
+
+public class DialogueEvent : GenericEvent
+{
+    public string[] m_dialogue = new string[0] { };
+}
+
+public class AnimationEvent : GenericEvent
+{
+    public Animator m_animator = null;
+    public string m_trigger = "";
+}
