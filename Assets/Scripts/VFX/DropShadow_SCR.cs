30a6012bc1da38c9535f20e500012e4d194ae332
sebbyling <43594362+sebbyling@users.noreply.github.com>
2020-07-20 13:24:00 +1200

    Drop Shadow Script
    Drop shadow script is now on the drop shadow prefab
    Had to reimport some UI due to transparency loss rip

---
+++ Assets/Scripts/VFX/DropShadow_SCR.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class DropShadow_SCR : MonoBehaviour
+{
+    [SerializeField] private LayerMask groundMask = 0;
+    private float yOffset = 0.1f;
+
+    // Update is called once per frame
+    void Update()
+    {
+        Ray ray;
+        RaycastHit hit;
+
+        if (Physics.Raycast(transform.position, Vector3.down, out hit, Mathf.Infinity, groundMask))
+        {
+            Vector3 newPos = new Vector3(hit.point.x, hit.point.y + yOffset, hit.point.z);
+
+            transform.position = newPos;
+        }
+    }
+}
