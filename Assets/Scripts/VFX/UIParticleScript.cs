f22ddd56bc85508a56341957f87b06d1072910b8
sebbyling <43594362+sebbyling@users.noreply.github.com>
2020-07-23 10:40:03 +1200

    UI Particles
    Added prefabs UI Particle System and UI Particle Base. Use UI Particle System on the Canvas for UI particle effects

---
+++ Assets/Scripts/VFX/UIParticleScript.cs
@@ -0,0 +1,90 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class UIParticleScript : MonoBehaviour
+{
+    private Image rend;
+    private Rigidbody2D rb;
+
+    [Header("Particle")]
+    public float startLifetime = 1.0f;
+    public float startSpeed = 1.0f;
+    public Vector3 startSize = new Vector3(1, 1, 1);
+    public float startRotation = 0.0f;
+    public float gravity = 0.0f;
+
+    [Space]
+
+    [Header("Colour Over Lifetime")]
+    public Gradient Colour;
+
+    [Space]
+    [Header("Size Over Lifetime")]
+    public AnimationCurve Size;
+
+    [Space]
+    [Header("Rotation Over Lifetime")]
+    public float minRotation;
+    public float maxRotation;
+    private float myRot;
+
+    [Space]
+    [Header("Renderer")]
+    public Material mat;
+
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        rend = GetComponent<Image>();
+        rb = GetComponent<Rigidbody2D>();
+
+        rb.gravityScale = gravity * 2;
+
+        rb.AddForce(transform.up * (startSpeed * 10), ForceMode2D.Impulse);
+        rend.material = mat; 
+
+        //Colour
+        rend.color = Colour.Evaluate(0.0f);
+
+        //Scale
+        transform.localScale = startSize;
+
+        //Rotation
+        transform.rotation = Quaternion.Euler(0, 0, startRotation);
+        myRot = Random.Range(minRotation, maxRotation);
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (startLifetime > 0)
+        {
+            startLifetime -= Time.deltaTime;
+        }
+        else
+        {
+            Destroy(this.gameObject);
+        }
+
+        //Colour
+        rend.color = Colour.Evaluate(1.0f / startLifetime);
+
+        //Rotation
+        if (myRot != 0)
+        {
+            transform.Rotate(0, 0, myRot * Time.deltaTime);
+        }
+
+        //Scale
+        float myScale = Size.Evaluate(1.0f / startLifetime);
+        transform.localScale = new Vector3(myScale, myScale, myScale);
+    }
+
+    private void OnBecameInvisible()
+    {
+        Destroy(this.gameObject);
+    }
+}
---
+++ Assets/Scripts/VFX/UIParticleSystemScript.cs
@@ -0,0 +1,92 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class UIParticleSystemScript : MonoBehaviour
+{
+    [Header("Particle")]
+    [SerializeField] private float Duration = 5.0f;
+    [SerializeField] private bool Looping = true;
+    [SerializeField] private float startLifetime = 1.0f;
+    [SerializeField] private float startSpeed = 1.0f;
+    [SerializeField] private Vector3 startSize = new Vector3(1, 1, 1);
+    [SerializeField] private float startRotation = 0.0f;
+    [SerializeField] private float gravity = 0.0f;
+
+    [Space]
+    [Header("Emission")]
+    [SerializeField] private float RateOverTime = 1.0f;
+    private float currentRate = 0.0f;
+
+    [Space]
+    [Header("Shape")]
+    [SerializeField] private float minX;
+    [SerializeField] private float maxX, minY, maxY;
+
+    [Space]
+
+    [Header("Colour Over Lifetime")]
+    //[SerializeField] private bool changeColour = false;
+    [SerializeField] private Gradient Colour;
+
+    [Space]
+    [Header("Size Over Lifetime")]
+    //[SerializeField] private bool changeSpeed = false;
+    [SerializeField] private AnimationCurve Size;
+
+    [Space]
+    [Header("Rotation Over Lifetime")]
+    //[SerializeField] private bool changeRotation = false;
+    [SerializeField] private float minRotation, maxRotation;
+
+    [Space]
+    [Header("Renderer")]
+    [SerializeField] private GameObject particleBase;
+    [SerializeField] private Material material;
+
+
+    void Update()
+    {
+        if (currentRate <= 0.0f)
+        {
+            CreateParticle();
+
+            currentRate = 1.0f / RateOverTime;
+        }
+        else
+        {
+            currentRate -= Time.deltaTime;
+        }
+
+        if (!Looping)
+        {
+            Duration -= Time.deltaTime;
+
+            if (Duration <= 0)
+            {
+                Destroy(this.gameObject);
+            }
+        }
+    }
+
+    void CreateParticle()
+    {
+        GameObject newParticle = Instantiate(particleBase, transform.position, transform.rotation) as GameObject;
+
+        newParticle.transform.SetParent(transform);
+
+        UIParticleScript ps = newParticle.GetComponent<UIParticleScript>();
+
+        ps.startLifetime = startLifetime;
+        ps.startSpeed = startSpeed;
+        ps.startSize = startSize;
+        ps.startRotation = startRotation;
+        ps.gravity = gravity;
+        ps.Colour = Colour;
+        ps.Size = Size;
+        ps.minRotation = minRotation;
+        ps.maxRotation = maxRotation;
+        ps.mat = material;
+    }
+}
