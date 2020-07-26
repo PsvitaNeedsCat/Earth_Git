ce0030e09c234bf2c1791a217ccde28a8a6edd2e
Tim <timbbby@gmail.com>
2020-07-27 09:50:08 +1200

    Cobra boss script

---
+++ Assets/Scripts/Bosses/Cobra/CobraShuffle.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CobraShuffle : CobraBehaviour
+{
+
+
+    public override void StartBehaviour()
+    {
+        base.StartBehaviour();
+    }
+
+    public override void CompleteBehaviour()
+    {
+        base.CompleteBehaviour();
+    }
+
+    public override void Reset()
+    {
+        base.Reset();
+    }
+}
