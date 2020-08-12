b11bb215a5981da819b7d376f7523733e11b890a
PsvitaNeedsCat <53197859+PsvitaNeedsCat@users.noreply.github.com>
2020-08-13 11:09:16 +1200

    Menu Sounds
    - Added sounds to main menu and pause menu

--- Assets/Scripts/Level/UI/ExitToOverworld.cs
+++ Assets/Scripts/Level/UI/ExitToOverworld.cs
@@ -3,7 +3,6 @@ using System.Collections.Generic;
 using UnityEngine;
 
 using UnityEngine.SceneManagement;
-using UnityEngine.SocialPlatforms.GameCenter;
 
 public class ExitToOverworld : MonoBehaviour
 {
--- Assets/Scripts/Messaging/MessageBus.cs
+++ Assets/Scripts/Messaging/MessageBus.cs
@@ -76,6 +76,8 @@ public enum EMessageType
     vulnerableStart,
     vulnerableEnd,
     gameOver,
+    snapSound,
+    switchSound,
 
     // Cobra boss
     cobraBarrageFire,
