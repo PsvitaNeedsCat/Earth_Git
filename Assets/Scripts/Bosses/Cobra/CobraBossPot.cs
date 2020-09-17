dfc9ea22282a4811806b4cbd09fe00ec52aeeb8a
Tim <timbbby@gmail.com>
2020-09-18 10:05:12 +1200

    Cobra boss pot damages

---
+++ Assets/Scripts/Bosses/Cobra/CobraBossPot.cs
@@ -0,0 +1,15 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CobraBossPot : MonoBehaviour
+{
+    private void OnCollisionEnter(Collision collision)
+    {
+        Player player = collision.gameObject.GetComponent<Player>();
+        if (player)
+        {
+            player.GetComponent<HealthComponent>().Health -= 1;
+        }
+    }
+}
