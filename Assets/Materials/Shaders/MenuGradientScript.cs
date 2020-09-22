a85152277fb8c22d05379e608b1d34669df2e5ee
Callum <53197859+PsvitaNeedsCat@users.noreply.github.com>
2020-09-22 17:17:54 +1200

    New Menu Assets
    - Added in new menu assets
    - Fixed bug with text fading

--- Assets/Materials/Shaders/MenuGradientScript.cs
+++ Assets/Materials/Shaders/MenuGradientScript.cs
@@ -4,32 +4,32 @@ using UnityEngine;
 
 public class MenuGradientScript : MonoBehaviour
 {
-    private MeshRenderer mr;
+    private MeshRenderer m_meshRenderer;
 
-    public Gradient gradient1;
-    public Gradient gradient2;
+    public Gradient m_gradient1;
+    public Gradient m_gradient2;
 
-    public float speedMultipler = 0.5f;
-    private float gradientTime = 0.0f;
+    public float m_speed = 0.5f;
+    private float m_gradientTime = 0.0f;
 
-    private Color c1;
-    private Color c2;
+    private Color m_colour1;
+    private Color m_colour2;
 
     // Start is called before the first frame update
-    void Start()
+    void Awake()
     {
-        mr = GetComponent<MeshRenderer>();
+        m_meshRenderer = GetComponent<MeshRenderer>();
     }
 
     // Update is called once per frame
     void Update()
     {
-        gradientTime = Mathf.PingPong(Time.time * speedMultipler, 1);
+        m_gradientTime = Mathf.PingPong(Time.time * m_speed, 1);
 
-        c1 = (gradient1.Evaluate(gradientTime));
-        mr.material.SetColor("_Color", c1);
+        m_colour1 = (m_gradient1.Evaluate(m_gradientTime));
+        m_meshRenderer.material.SetColor("_Color", m_colour1);
 
-        c2 = (gradient2.Evaluate(gradientTime));
-        mr.material.SetColor("_Color1", c2);
+        m_colour2 = (m_gradient2.Evaluate(m_gradientTime));
+        m_meshRenderer.material.SetColor("_Color1", m_colour2);
     }
 }
