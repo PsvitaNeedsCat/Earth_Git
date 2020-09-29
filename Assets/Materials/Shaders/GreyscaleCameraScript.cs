77f38bafe245b4748fdb1224d08c556b7dd39287
sebbyling <43594362+sebbyling@users.noreply.github.com>
2020-09-30 11:52:38 +1300

    Added Greyscale Capability
    Debug tool to check scene contrast

---
+++ Assets/Materials/Shaders/GreyscaleCameraScript.cs
@@ -0,0 +1,30 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class GreyscaleCameraScript : MonoBehaviour
+{
+    public Material mat;
+    [SerializeField] private bool turnGrey = false;
+
+    void Start()
+    {
+        mat.SetFloat("_Power", 0.0f);
+    }
+
+    private void Update()
+    {
+        if (turnGrey)
+        {
+            mat.SetFloat("_Power", 1.0f);
+        }
+        else
+        {
+            mat.SetFloat("_Power", 0.0f);
+        }
+    }
+
+    void OnRenderImage(RenderTexture source, RenderTexture destination)
+    {
+        Graphics.Blit(source, destination, mat);
+    }
+}
