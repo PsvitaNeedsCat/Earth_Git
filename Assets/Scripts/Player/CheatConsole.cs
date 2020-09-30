113cbed2d38aba60c718fd5726cab6cd38f33cd6
Tim <timbbby@gmail.com>
2020-09-30 16:32:48 +1300

    Added help to cheat console

--- Assets/Scripts/Player/CheatConsole.cs
+++ Assets/Scripts/Player/CheatConsole.cs
@@ -5,7 +5,9 @@ using System.Linq;
 
 public class CheatConsole : MonoBehaviour
 {
-    bool m_showConsole = false;
+    private bool m_showConsole = false;
+    private bool m_showHelp = false;
+    private Vector2 m_scroll;
 
     string m_input = "";
 
@@ -14,11 +16,14 @@ public class CheatConsole : MonoBehaviour
     public static CheatCommand<int> SET_CUR_HEALTH;
     public static CheatCommand<int> SET_MAX_HEALTH;
     public static CheatCommand<EChunkEffect> TOGGLE_POWER;
+    public static CheatCommand HELP;
 
     public List<object> m_commandList;
 
     private PlayerController m_playerController;
     private Player m_player;
+
+    private bool m_justOpened = false;
 
     public void OnReturn()
     {
@@ -32,6 +37,12 @@ public class CheatConsole : MonoBehaviour
     public void OnToggleDebug()
     {
         m_showConsole = !m_showConsole;
+
+        if (m_showConsole)
+        {
+            m_justOpened = true;
+            m_input = "";
+        }
     }
 
     private void Awake()
@@ -64,13 +75,19 @@ public class CheatConsole : MonoBehaviour
             m_player.TogglePower(x);
         });
 
+        HELP = new CheatCommand("help", "Shows a list of commands", "help", () =>
+        {
+            m_showHelp = !m_showHelp;
+        });
+
         m_commandList = new List<object>
         {
             AAAA,
             OCEAN_MAN,
             SET_CUR_HEALTH,
             SET_MAX_HEALTH,
-            TOGGLE_POWER
+            TOGGLE_POWER,
+            HELP    
         };
     }
 
@@ -82,10 +99,41 @@ public class CheatConsole : MonoBehaviour
         }
 
         float consoleHeight = 0.0f;
+        
+        if (m_showHelp)
+        {
+            GUI.Box(new Rect(0, consoleHeight, Screen.width, 100), "");
+
+            Rect viewport = new Rect(0, 0, Screen.width - 30, 20 * m_commandList.Count);
+
+            m_scroll = GUI.BeginScrollView(new Rect(0, consoleHeight + 5.0f, Screen.width, 90), m_scroll, viewport);
+
+            for (int i = 0; i < m_commandList.Count; i++)
+            {
+                CheatCommandBase command = m_commandList[i] as CheatCommandBase;
+                string label = $"{command.m_commandFormat} - {command.m_commandDescription}";
+                Rect labelRect = new Rect(5, 20 * i, viewport.width - 100, 20);
+
+                GUI.Label(labelRect, label);
+            }
+
+            GUI.EndScrollView();
+            consoleHeight += 100.0f;
+        }        
 
         GUI.Box(new Rect(0, consoleHeight, Screen.width, 30), "");
         GUI.backgroundColor = new Color(0, 0, 0, 0);
+
+        GUI.SetNextControlName("ConsoleInput");
         m_input = GUI.TextField(new Rect(10.0f, consoleHeight + 5.0f, Screen.width - 20.0f, 20.0f), m_input);
+
+        // On opened
+        if (m_justOpened)
+        {
+            m_justOpened = false;
+
+            GUI.FocusControl("ConsoleInput");
+        }
     }
 
     private void HandleInput()
