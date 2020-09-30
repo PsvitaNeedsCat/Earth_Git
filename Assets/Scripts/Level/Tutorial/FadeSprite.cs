dadbb2b7ae7a193199407dc8c6ab3403da3be4c8
Callum <53197859+PsvitaNeedsCat@users.noreply.github.com>
2020-10-01 12:37:21 +1300

    Tutorial Expo
    - Added story at the end of tutorial

---
+++ Assets/Scripts/Level/Tutorial/FadeSprite.cs
@@ -0,0 +1,27 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+using DG.Tweening;
+
+[RequireComponent(typeof(SpriteRenderer))]
+public class FadeSprite : MonoBehaviour
+{
+    private SpriteRenderer m_renderer = null;
+
+    private void Awake()
+    {
+        m_renderer = GetComponent<SpriteRenderer>();
+    }
+
+    // Fades the sprite in
+    public void FadeIn(float _duration)
+    {
+        m_renderer.DOFade(1.0f, _duration);
+    }
+
+    public void FadeOut(float _duration)
+    {
+        m_renderer.DOFade(0.0f, _duration);
+    }
+}
