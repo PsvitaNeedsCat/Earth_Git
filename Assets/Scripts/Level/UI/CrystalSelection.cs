df915c88901929cb3fda227bbea409da11dd8cfd
Callum <53197859+PsvitaNeedsCat@users.noreply.github.com>
2020-10-12 11:06:59 +1300

    Crystal Selection
    - Updated crystal selection so that colours are darker when not selected

--- Assets/Scripts/Level/UI/CrystalSelection.cs
+++ Assets/Scripts/Level/UI/CrystalSelection.cs
@@ -1,9 +1,6 @@
-﻿using System.Collections;
-using System.Collections.Generic;
+﻿using DG.Tweening;
 using UnityEngine;
-
 using UnityEngine.UI;
-using DG.Tweening;
 
 public class CrystalSelection : MonoBehaviour
 {
@@ -11,9 +8,18 @@ public class CrystalSelection : MonoBehaviour
     [SerializeField] private Image m_backgroundImage;
     [SerializeField] private Image[] m_gemSprites;
 
+    private int m_currentlySelected = 0;
+
+    private float m_selectedLightValue = 1.0f;
+    private float m_defaultLightValue = 0.5f;
+
     public void UpdateSelected(int _selected, bool _silent = false)
     {
         m_backgroundImage.sprite = m_backgroundSprites[_selected];
+
+        SetGemColour(m_currentlySelected, false);
+        SetGemColour(_selected, true);
+        m_currentlySelected = _selected;
 
         if (!_silent)
         {
@@ -22,12 +28,23 @@ public class CrystalSelection : MonoBehaviour
         }
     }
 
+    private void SetGemColour(int _gem, bool _selected)
+    {
+        Color newColour = m_gemSprites[_gem].color;
+        float h, s, v;
+        Color.RGBToHSV(newColour, out h, out s, out v);
+        v = (_selected) ? m_selectedLightValue : m_defaultLightValue;
+        newColour = Color.HSVToRGB(h, s, v);
+        m_gemSprites[_gem].color = newColour;
+    }
+
     public void UpdateUnlocked(bool[] _active)
     {
         // Enable the images when they are unlocked
         for (int i = 0; i < m_gemSprites.Length; i++)
         {
             m_gemSprites[i].enabled = _active[i];
+            SetGemColour(i, (m_currentlySelected == i));
         }
     }
 }
