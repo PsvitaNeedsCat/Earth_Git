61031deb3eae729a0d07e9628a3f660e538a6745
Tim <timbbby@gmail.com>
2020-10-16 17:23:02 +1300

    Fire charger bug death work

--- Assets/Scripts/Managers & Handlers/EffectsManager.cs
+++ Assets/Scripts/Managers & Handlers/EffectsManager.cs
@@ -19,6 +19,8 @@ public class EffectsManager : MonoBehaviour
         centipedeHeadDeath,
         centipedeBodyDeath,
         centipedeTailDeath,
+        fireChargerDeath,
+        fireGrubDeath,
         cobraPotLand,
         waterProjectileDestroyed,
         rockToadProjectileDestroyed,
