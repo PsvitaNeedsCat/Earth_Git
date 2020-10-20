c8d26fce840a93f11276c0cc105aef9c5bb1a3d1
Tim <timbbby@gmail.com>
2020-10-21 12:59:12 +1300

    Toad wave scales

--- Assets/Scripts/Bosses/Toad/ToadWave.cs
+++ Assets/Scripts/Bosses/Toad/ToadWave.cs
@@ -7,6 +7,7 @@ public class ToadWave : MonoBehaviour
 {
     public float m_travelTime = 3.0f;
     public float m_travelDist = 12.0f;
+    public AnimationCurve m_waveHeightCurve;
 
     public void Launch()
     {
@@ -15,6 +16,27 @@ public class ToadWave : MonoBehaviour
         ActivateChildren();
 
         transform.DOMoveZ(transform.position.z + m_travelDist, m_travelTime).OnComplete(() => WaveEnd());
+        transform.localScale = new Vector3(1.0f, 0.1f, 1.0f);
+
+        // transform.DOPunchScale(new Vector3(0.0f, 2.0f, 0.0f), m_travelTime, 0, 0).SetEase(Ease.InOutCubic);
+        StartCoroutine(SetWaveHeight());
+    }
+
+    private IEnumerator SetWaveHeight()
+    {
+        float timer = 0.0f;
+
+        while (timer < m_travelTime)
+        {
+            timer += Time.deltaTime;
+
+            float sampleTime = Mathf.Clamp01(timer / m_travelTime);
+            Vector3 localScale = transform.localScale;
+            localScale.y = m_waveHeightCurve.Evaluate(sampleTime);
+            transform.localScale = localScale;
+
+            yield return null;
+        }
     }
 
     void WaveEnd()
